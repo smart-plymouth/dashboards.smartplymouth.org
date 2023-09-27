f226d714fd4a78a6627869b6a6f23ace406a1a0f
Robert Putt <Robert.Putt@C02FV3BTMD6R.local>
2023-09-27 21:53:03 +0100

    Add date state to Beryl component

--- src/routes/berylbikes.js
+++ src/routes/berylbikes.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 
 const BerylBikes = () => {
- return (
-    <>
-        <div>Beryl Bikes!</div>
-    </>
- )
-}
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
 
+  return (
+    <>
+      <div>Beryl Bikes!</div>
+      <div>{ selectedDate.toString() }</div>
+    </>
+  )
+}
 
 export default BerylBikes;
